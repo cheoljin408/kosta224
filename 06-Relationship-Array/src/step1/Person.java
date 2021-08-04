0f46134b603f54867989d08dd0381bd21972c7f9
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2021-08-04 10:52:56 +0900

    Association 예제

--- 06-Relationship-Array/src/step1/Person.java
+++ 06-Relationship-Array/src/step1/Person.java
@@ -11,6 +11,13 @@ public class Person {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+	/*
+	 	association 즉 use a 관계는 대상 객체를 사용하는 관계
+	 	아래와 같이 대상 객체(RentCar)를 지역변수local variable로 선언, 생성하여
+	 	사용하고 메소드가 종료되면 메모리에서 해제되도록 구현한다.
+	 */
+	public void tour() {
+		RentCar car = new RentCar("테슬라", "검정");
+		System.out.println(name + " " + car.getColor() +"색 " + car.getModel() + " 렌트카 타고 여행하다.");
+	}
 }
