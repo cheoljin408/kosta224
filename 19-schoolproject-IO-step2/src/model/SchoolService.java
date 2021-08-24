be89d8bcc56bee6ba46cfccaa51772e05664df28
cheoljin408 <52393564+cheoljin408@users.noreply.github.com>
2021-08-24 14:10:55 +0900

    loadData(), saveData() 메소드 추가

--- 19-schoolproject-IO-step2/src/model/SchoolService.java
+++ 19-schoolproject-IO-step2/src/model/SchoolService.java
@@ -1,5 +1,12 @@
 package model;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
@@ -7,6 +14,56 @@ import java.util.LinkedHashMap;
 public class SchoolService {
 	// key: tel value: Member 하위
 	private LinkedHashMap<String, Member> map = new LinkedHashMap<String, Member>();
+	private String path = "C:\\kosta224\\iotest\\school\\member-list.obj";
+	
+	/**
+	 * 객체 역직렬화하여 파일에서 구성원 정보를 로드하는 메소드
+	 * 
+	 * FileInputStream < ObjectInputStream : readObject() -> instance variable map에 할당
+	 * 
+	 * tip) 프로그램 첫 시작 시점에는 파일이 없으므로 if(file.isFile()) {객체 역직렬화}
+	 * @throws IOException 
+	 * @throws FileNotFoundException 
+	 * @throws ClassNotFoundException 
+	 */
+	@SuppressWarnings("unchecked")
+	public void loadData() throws FileNotFoundException, IOException, ClassNotFoundException {
+		File file = new File(path);
+		if(file.isFile()) { // 파일이 존재하면 객체 역직렬화 작업을 수행한다
+			ObjectInputStream ois = null;
+			try {
+				ois = new ObjectInputStream(new FileInputStream(path));
+				map = (LinkedHashMap<String, Member>) ois.readObject();
+			} finally {
+				if(ois != null)
+					ois.close();
+			}
+		}
+	}
+	
+	/**
+	 * 객체 직렬화하여 파일에 구성원 정보를 저장하는 메소드
+	 * 
+	 * FileOutputStream < ObjectOutputStream : writeObject(map)
+	 * 
+	 * tip) 저장하려는 파일의 디렉토리가 존재하지 않을때는 디렉토리를 생성한다 mkdirs()
+	 * @throws IOException 
+	 * @throws FileNotFoundException 
+	 */
+	public void saveData() throws FileNotFoundException, IOException {
+		File dir = new File(path).getParentFile();
+		if(dir.isDirectory() == false) {
+			dir.mkdirs();
+		}
+		ObjectOutputStream oos = null;
+		try {
+			oos = new ObjectOutputStream(new FileOutputStream(path));
+			oos.writeObject(map);
+		} finally {
+			if(oos != null)
+				oos.close();
+		}
+	}
 	
 	public void addMember(Member member) throws DuplicateTelException {
 		if(map.containsKey(member.getTel())) {
